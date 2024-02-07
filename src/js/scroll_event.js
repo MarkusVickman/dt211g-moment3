26681413315aebbc0feff48948b752ea3e64045e
Markus <markus.vickman@hotmail.com>
2024-02-07 16:10:11 +0100

    animering på rubrik. Testat lite med onscroll i JS

---
+++ src/js/scroll_event.js
@@ -0,0 +1,8 @@
+let arrowEl = document.getElementById("arrow");
+
+document.addEventListener("scroll", function (e) {    
+    arrowEl.style.display = "block";
+    /*setTimeout(function () {
+        arrowEl.style.display = "none";
+      }, 5000);   */
+})
