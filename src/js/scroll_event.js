7825393b7a116d54402c028ddc08b659fca4701b
Markus Vickman~ <markus.vickman@hotmail.com>
2024-02-10 23:05:07 +0100

    snyggat till js för scollbar

--- src/js/scroll_event.js
+++ src/js/scroll_event.js
@@ -6,15 +6,11 @@ let bar2 = document.getElementById("scrollbar2");
 let bar3 = document.getElementById("scrollbar3");
 let bar4 = document.getElementById("scrollbar4");
 let bar5 = document.getElementById("scrollbar5");
-let bar6 = document.getElementById("scrollbar6");
-let bar7 = document.getElementById("scrollbar7");
-let bar8 = document.getElementById("scrollbar8");
-let bar9 = document.getElementById("scrollbar9");
-let bar10 = document.getElementById("scrollbar10");
 let scrollBar = document.getElementById("scrollbar");
 
 //Används för att pilen till som visas vid skroll för att den inte ska bugga
 let blocker = 1;
+let blocker2 = 1;
 
 // variable som lagrar den längden som dokumentet kan skrollas
 let scrollPotential = (document.documentElement.scrollHeight/*documentets totala höjd*/ - window.innerHeight/*fönstrets höjd*/);
@@ -32,6 +28,29 @@ window.onscroll = () => {
     }, 5000);
   }
 
+  //En insats som visar en pil och sätter display none efter en timer
+  if (window.innerWidth < 700) {
+    if (blocker2 === 1) {
+      blocker2 = 0;
+      scrollBar.style.display = "block";
+      setTimeout(function () {
+        scrollBar.style.display = "none";
+        blocker2 = 1;
+      }, 5000);
+    }
+
+    indicateScroll();
+  } else {
+
+    indicateScroll();
+  }
+}
+
+/*Inte det snyggaste kanske men jag försökte
+En if och en else sats för varje av de fem rutorna till scrollindikering på hemsidan
+If-satserna kontrollerar längd från toppen av skärmen och ändrar stil utifrån det.*/
+function indicateScroll() {
+
   //Värden för att avgöra vilken ruta som ska ändra färg
   let five = scrollPotential;
   let oneOfFive = (five / 5);
@@ -40,69 +59,29 @@ window.onscroll = () => {
   let fourOfFive = (oneOfFive * 4);
 
 
-  //En insats som visar en pil och sätter display none efter en timer
-  if (window.innerWidth < 700) {
-
-    scrollBar.style.display = "block";
-    setTimeout(function () {
-      scrollBar.style.display = "none";
-    }, 5000);
-
-    /*Inte det snyggaste kanske men jag försökte
-    En if och en else sats för varje av de fem rutorna till scrollindikering på hemsidan
-    If-satserna kontrollerar längd från toppen av skärmen och ändrar stil utifrån det.*/
-    if ((document.documentElement.scrollTop) < oneOfFive) {
-      bar1.style.backgroundColor = "darkred";
-    } else {
-      bar1.style.backgroundColor = "transparent";
-    }
-    if (document.documentElement.scrollTop < twoOfFive && document.documentElement.scrollTop > oneOfFive) {
-      bar2.style.backgroundColor = "darkred";
-    } else {
-      bar2.style.backgroundColor = "transparent";
-    }
-    if (document.documentElement.scrollTop < treeOfFive && document.documentElement.scrollTop > twoOfFive) {
-      bar3.style.backgroundColor = "darkred";
-    } else {
-      bar3.style.backgroundColor = "transparent";
-    }
-    if (document.documentElement.scrollTop < fourOfFive && document.documentElement.scrollTop > treeOfFive) {
-      bar4.style.backgroundColor = "darkred";
-    } else {
-      bar4.style.backgroundColor = "transparent";
-    }
-    if (document.documentElement.scrollTop > fourOfFive) {
-      bar5.style.backgroundColor = "darkred";
-    } else {
-      bar5.style.backgroundColor = "transparent";
-    }
+  if ((document.documentElement.scrollTop) < oneOfFive) {
+    bar1.style.backgroundColor = "darkred";
+  } else {
+    bar1.style.backgroundColor = "transparent";
   }
-  else {
-    if ((document.documentElement.scrollTop) < oneOfFive) {
-      bar1.style.backgroundColor = "darkred";
-    } else {
-      bar1.style.backgroundColor = "transparent";
-    }
-    if (document.documentElement.scrollTop < twoOfFive && document.documentElement.scrollTop > oneOfFive) {
-      bar2.style.backgroundColor = "darkred";
-    } else {
-      bar2.style.backgroundColor = "transparent";
-    }
-    if (document.documentElement.scrollTop < treeOfFive && document.documentElement.scrollTop > twoOfFive) {
-      bar3.style.backgroundColor = "darkred";
-    } else {
-      bar3.style.backgroundColor = "transparent";
-    }
-    if (document.documentElement.scrollTop < fourOfFive && document.documentElement.scrollTop > treeOfFive) {
-      bar4.style.backgroundColor = "darkred";
-    } else {
-      bar4.style.backgroundColor = "transparent";
-    }
-    if (document.documentElement.scrollTop > fourOfFive) {
-      bar5.style.backgroundColor = "darkred";
-    } else {
-      bar5.style.backgroundColor = "transparent";
-    }
-
+  if (document.documentElement.scrollTop < twoOfFive && document.documentElement.scrollTop > oneOfFive) {
+    bar2.style.backgroundColor = "darkred";
+  } else {
+    bar2.style.backgroundColor = "transparent";
+  }
+  if (document.documentElement.scrollTop < treeOfFive && document.documentElement.scrollTop > twoOfFive) {
+    bar3.style.backgroundColor = "darkred";
+  } else {
+    bar3.style.backgroundColor = "transparent";
+  }
+  if (document.documentElement.scrollTop < fourOfFive && document.documentElement.scrollTop > treeOfFive) {
+    bar4.style.backgroundColor = "darkred";
+  } else {
+    bar4.style.backgroundColor = "transparent";
+  }
+  if (document.documentElement.scrollTop > fourOfFive) {
+    bar5.style.backgroundColor = "darkred";
+  } else {
+    bar5.style.backgroundColor = "transparent";
   }
 }
